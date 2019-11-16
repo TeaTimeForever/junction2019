a02f93922eba017c78000b2a5606a57200168c7c
karina <k.pilyushonoka@gmail.com>
2019-11-16 19:03:05 +0200

    Deploy the cloud function successfully

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,50 @@
 import * as functions from 'firebase-functions';
+import * as  admin from 'firebase-admin';
+import { UserChallenge } from './user';
+import { DocumentData } from '@google-cloud/firestore';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+
+export const getChallenge = functions.https.onRequest(async (request, response) => {
+  const uid = request.param('uid');
+  response.set('Access-Control-Allow-Origin', '*');
+  response.set('Content-Type', 'application/json');
+
+  const userSn = await db.doc(`users/${uid}`).get();
+  if (!userSn.exists) {
+    response.statusCode = 404;
+    response.send();
+  } else {
+    const userChallengesSn = await db.collection(`users/${uid}/challenges`).get();
+
+    const alreadyChanllenged: string[] = [];
+    if (!userChallengesSn.empty) {
+      userChallengesSn.docs.forEach(userChSn => {
+        alreadyChanllenged.push(userChSn.id);
+      });
+    }
+
+    let res: DocumentData | null = null;
+    const allChallengesSn = (await db.collection(`gettingToNo/general/challenges`).get()).docs;
+    allChallengesSn.forEach(async challengeSn => {
+      if (!alreadyChanllenged.includes(challengeSn.id) && !res) {
+        res = challengeSn.data();
+        userSn.ref.set({activeChallenge: challengeSn.id});
+      }
+    });
+
+    if (!res) {
+      userChallengesSn.forEach(userChSn => {
+        if (!res) {
+          const userCh = userChSn.data() as UserChallenge;
+          if (userCh.status === 'accepted') {
+            console.log('not implemented yet')
+          }
+        }
+      });
+    }
+
+    response.send(JSON.stringify(res));
+  }
+ });
