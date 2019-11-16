e317a00c34538ce8d7e880a6052e01a783c23b78
karina <k.pilyushonoka@gmail.com>
2019-11-16 18:12:24 +0200

    function

---
+++ functions/user.ts
@@ -0,0 +1,16 @@
+import { Timestamp } from '@google-cloud/firestore';
+
+export interface User {
+  activeChallenge: string;
+}
+
+export interface UserChallenge {
+  status: 'accepted' | 'rejected' | 'done';
+  started_at: Timestamp;
+  finished_at: Timestamp;
+}
+
+export interface Challenge {
+  imgUrl: string;
+  question: string;
+}
